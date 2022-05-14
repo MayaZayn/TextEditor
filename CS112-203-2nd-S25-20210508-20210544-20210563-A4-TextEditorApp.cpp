6b313bf82d58445ce2184e8befc04a9b045779f6
MayaZayn <102183224+MayaZayn@users.noreply.github.com>
2022-05-14 22:01:56 +0200

    Added (main, source, and header) with the right names

---
+++ CS112-203-2nd-S25-20210508-20210544-20210563-A4-TextEditorApp.cpp
@@ -0,0 +1,89 @@
+#include "CS112-203-2nd-S25-20210508-20210544-20210563-A4-TextEditorFunctions.h"
+
+int main() {
+    string option;
+    char filename[151];
+    cout << "Please enter the name of the text file you want to open:" << endl;
+    cin >> filename;
+    strcat_s(filename, ".txt");
+    fstream database (filename, ios::in);
+
+    if (database.fail()){
+        database.open(filename, ios::app);
+        cout << "This is a new file. I have created it for you.\n------------------------------\nWhat do you want to do today?" << endl;
+    }
+    else{
+        cout << "This file already exists.\nI have opened it for you.\n------------------------------\nWhat do you want to do today?" << endl;
+    }
+
+    database.close();
+
+    loadFileContent(filename);
+
+    while (true){
+        menu();
+        cin.clear();
+        cin >> option;
+        cin.ignore();
+        if (option == "1"){
+            addTxt();
+        }
+        else if (option == "2"){
+            displayContent();
+        }
+        else if (option == "3"){
+            emptyFile(filename);
+        }
+        else if (option == "4"){
+            encrypt();
+        }
+        else if (option == "5"){
+            decrypt();
+        }
+        else if (option == "6"){
+            mergeAnotherFile();
+        }
+        else if (option == "7"){
+            countWords();
+        }
+        else if (option == "8"){
+            countChars();
+        }
+        else if (option == "9"){
+            countLines();
+        }
+        else if (option == "10"){
+            searchWord();
+        }
+        else if (option == "11"){
+            wordCount();
+        }
+        else if (option == "12") {
+            allUpper();
+        }
+        else if (option == "13") {
+            tolower(fileContent);
+            printf("The contents of the file has been transformed to lower case!\n");
+        }
+        else if (option == "14") {
+            firstUpper();
+        }
+        else if (option == "15") {
+            saveFileContent(filename);
+        }
+        else if (option == "16") {
+            break;
+        }
+        else {
+            cout << "Please choose a valid option!\n";
+            sleep(1);
+            continue;
+        }
+
+        cout << "\n-----------------------------";
+        cout << "\nWhat else do you want to do?\n\n";
+    }
+    cout << "Bye Bye User...\n"; // say bye bye
+    sleep(2);
+    return 0;
+}
---
+++ CS112-203-2nd-S25-20210508-20210544-20210563-A4-TextEditorFunctions.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_CPP_SOURCE_H
+#define MAIN_CPP_SOURCE_H
+
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <conio.h>
+#include <sstream>
+#include <unistd.h>
+#include <vector>
+
+using namespace std;
+
+extern string fileContent;
+
+void menu();
+void loadFileContent(string);
+string takeInput();
+void addTxt();
+void displayContent();
+void emptyFile(char file[151]);
+void encrypt();
+void decrypt();
+void mergeAnotherFile();
+void countWords();
+void countChars();
+void countLines();
+void searchWord();
+void wordCount();
+void allUpper();
+void tolower(string&);
+void firstUpper();
+void saveFileContent(string);
+
+#endif //MAIN_CPP_SOURCE_H
