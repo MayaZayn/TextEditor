f8be0aa7cbfd02dc382cd7b29adb0e5c555c9def
DarkenSoda <mahmoudadel200190@yahoo.com>
2022-05-16 18:06:16 +0200

    Fixed out of boundary in firstUpper

--- CS112-203-2nd-S25-20210508-20210544-20210563-A4-TextEditorFunctions.cpp
+++ CS112-203-2nd-S25-20210508-20210544-20210563-A4-TextEditorFunctions.cpp
@@ -220,7 +220,7 @@ void allUpper() {
 void firstUpper() {
     tolower(fileContent);       // turns entire string to lower
     fileContent[0] = toupper(fileContent[0]);       // makes first letter Cap
-    for (int i = 0; i <= fileContent.length(); i++) {
+    for (int i = 0; i < fileContent.length(); i++) {
         if (fileContent[i] == ' ' || fileContent[i] == '\n') {
             fileContent[i + 1] = toupper(fileContent[i + 1]);   // if a space or newline is found
         }                                                       // the character after it is turned Cap
